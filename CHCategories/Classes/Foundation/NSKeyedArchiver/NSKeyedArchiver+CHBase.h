36f08c45dbd01a5b2a99d552501010f89a7c6f61
colin <colin@colins-MacBook-Pro.local>
2020-03-06 21:53:59 +0800

    修复警告问题

--- CHCategories/Classes/Foundation/NSKeyedArchiver/NSKeyedArchiver+CHBase.h
+++ CHCategories/Classes/Foundation/NSKeyedArchiver/NSKeyedArchiver+CHBase.h
@@ -19,7 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
  @param exception return前调用, 若exception存在且指针非NULL, 存在异常
  @return 解档对象
  */
-+ (nullable id)ch_unarchiveObjectWithData:(NSData *)data exception:(__autoreleasing NSException **)exception;
++ (nullable id)ch_unarchiveObjectWithData:(NSData *)data exception:(__autoreleasing NSException *_Nullable *_Nullable)exception;
 
 /**
  根据归档对象文件路径, 解档对象(类同unarchiveObjectWithFile:方法, 区别于可传入异常对象)
@@ -28,7 +28,7 @@ NS_ASSUME_NONNULL_BEGIN
  @param exception return前调用, 若exception存在且指针非NULL, 存在异常
  @return 解档对象
  */
-+ (nullable id)ch_unarchiveObjectWithFile:(NSString *)path exception:(__autoreleasing NSException **)exception;
++ (nullable id)ch_unarchiveObjectWithFile:(NSString *)path exception:(__autoreleasing NSException *_Nullable *_Nullable)exception;
 
 @end
 
